12ae97b20db9233bda0eedf5948fad6a191f7f21
Mates0 <matyas.vanko@educanet.cz>
2021-12-09 18:22:48 +0100

    kotlin final

--- src/main/kotlin/processing/data_processing.kt
+++ src/main/kotlin/processing/data_processing.kt
@@ -1,42 +1,67 @@
 package processing
 
-import streams.*
+import streams.Gender
+import streams.Grade
+import streams.GradeType
+import streams.Student
 
 
 fun atLeastOneGradeA(student: Student): Boolean {
-    TODO()
+    return student.grades.any { grade ->
+        GradeType.A == grade.type
+    }
 }
 
 
 fun getStudentAges(students: List<Student>): List<Int> {
-    TODO()
+    return students.map { obj ->
+        obj.age
+    }
 }
 
 fun getStudentsWithMinimumAge(students: List<Student>, minAge: Int): List<Student> {
-    TODO()
+    return students.filter { student ->
+        minAge <= student.age
+    }
 }
 
 
 // gender == Gender.MALE
 // or gender.name == "MALE"
 fun countMaleStudents(students: List<Student>): Int {
-    TODO()
+    return students.count { student ->
+        Gender.MALE == student.gender
+    }
 }
 
 
 // gender == Gender.FEMALE
 // or gender.name == "FEMALE"
 fun avgAgeOfFemaleStudent(students: List<Student>): Double {
-    TODO()
+    return students.filter { student ->
+        Gender.FEMALE == student.gender
+    }
+        .map { obj: Student ->
+            obj.age
+        }.average()
 }
 
 fun getProductOfStudentAges(students: List<Student>): Int {
-    TODO()
+    return students.map { obj: Student -> obj.age }
+        .reduce { firstStudent, secondStudent ->
+            firstStudent * secondStudent
+        }
 }
 
 // ignore F Grades
 fun productOfStudentGrades(student: Student): Int {
-    TODO()
+    return student.grades.filter { grade ->
+        GradeType.F != grade.type
+    }.map { grade ->
+        grade.type.value
+    }.reduce { firstStudent, secondStudent ->
+        firstStudent * secondStudent
+    }
 }
 
 // region BONUS
